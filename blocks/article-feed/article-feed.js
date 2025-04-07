ba3ef59977cf2bcd95605bdd570016c0788a165b
oliver <o.kohl@sap.com>
2025-04-07 15:58:12 +0200

    new block article-feed
    - new block for list of articles (news)
    - faintly and ffetch added

---
+++ blocks/article-feed/article-feed.js
@@ -0,0 +1,19 @@
+import { renderBlock } from '../../src/scripts/faintly.js';
+
+async function fetchArticles() {
+  const response = await fetch(`${window.hlx.codeBasePath}/blocks/article-feed/articles.json`);
+  const json = await response.json();
+
+  return json.data;
+}
+
+export default async function decorate(block) {
+  await renderBlock(block, {
+    fetchArticles,
+    isFeaturedArticle: (context) => context.article.featured,
+    articleLinkAttrs: (context) => ({
+      href: context.article.link,
+      title: context.article.title,
+    }),
+  })
+}

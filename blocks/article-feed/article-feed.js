37e43c8e34e8e5f1e9390254078233694c2ec771
oliver <gokohl@gmail.com>
2025-05-01 17:36:32 +0200

    article feed styles
    - configurable index for articles
    - style improvements (without image)

--- blocks/article-feed/article-feed.js
+++ blocks/article-feed/article-feed.js
@@ -1,6 +1,6 @@
 import { renderBlock } from '../../scripts/faintly.js';
 import { readBlockConfig } from '../../scripts/aem.js';
-import { getIndexPath, INDEX_ALL } from '../../scripts/index-utils.js';
+import getIndexPath from '../../scripts/index-utils.js';
 import ffetch from '../../scripts/ffetch.js';
 
 function fetchArticlesFactory(block) {
@@ -8,8 +8,9 @@ function fetchArticlesFactory(block) {
     const config = readBlockConfig(block);
     const limit = parseInt(config?.limit ?? '', 10) || undefined;
     const type = config?.type?.trim().toLowerCase();
+    const index = config?.index?.trim().toUpperCase();
 
-    const rawEntries = await ffetch(getIndexPath(INDEX_ALL)).all();
+    const rawEntries = await ffetch(getIndexPath(`INDEX_${index}`)).all();
     const entries = rawEntries
       .filter((entry) => entry.type.toLowerCase() === type)
       .sort((a, b) => {
@@ -18,6 +19,11 @@ function fetchArticlesFactory(block) {
         return dateB - dateA; // descending
       })
       .slice(0, Number.isNaN(limit) ? undefined : limit);
+    entries.forEach((entry) => {
+      if (entry.path) {
+        entry.url = entry.path;
+      }
+    });
     return entries;
   };
 }
--- scripts/index-utils.js
+++ scripts/index-utils.js
@@ -1,7 +1,10 @@
 import { getLocale } from './i18n-utils.js';
 
-export const INDEX_ALL = 'query-index';
-export const INDEX_NEWS = 'query-index';
+const INDEX_PATHS = {
+  INDEX_ALL: 'query-index',
+  INDEX_NEWS: 'query-index',
+  INDEX_MENTIONS: 'press/in-the-news',
+};
 
 /**
  * Retrieves the path for a specified index, optionally including a country-specific prefix.
@@ -10,9 +13,11 @@ export const INDEX_NEWS = 'query-index';
  * @return {string} The constructed path for the given index. If a country is detected,
  * it includes a country-specific prefix; otherwise, it defaults to a general path.
  */
-export function getIndexPath(index) {
+function getIndexPath(index) {
   const language = getLocale();
   return (language !== '')
-    ? `${window.hlx.codeBasePath}/${language}/${index}.json`
-    : `${window.hlx.codeBasePath}/${index}.json`;
+    ? `${window.hlx.codeBasePath}/${language}/${INDEX_PATHS[index]}.json`
+    : `${window.hlx.codeBasePath}/${INDEX_PATHS[index]}.json`;
 }
+
+export default getIndexPath;

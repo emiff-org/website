f78a70a5ea3ff322eae949a05938d98d619f7e64
oliver <gokohl@gmail.com>
2025-05-05 23:30:20 +0200

    gallery modal
    - modal block added
    - dummy content
    - layout needs fixing

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,6 +1,30 @@
 import { createOptimizedPicture, readBlockConfig } from '../../scripts/aem.js';
 import ffetch from '../../scripts/ffetch.js';
 
+/**
+ * Builds modal content for data from given element.
+ * This assume certain dataset attributes to be available in the element.
+ * @param {Element} element The element with modal dataset
+ */
+export async function createModalContent(element) {
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(
+    createOptimizedPicture(element.dataset.image, element.dataset.description, false),
+  );
+  const description = document.createElement('p');
+  description.textContent = element.dataset.description;
+  fragment.appendChild(description);
+  const date = document.createElement('p');
+  date.textContent = `Date: ${element.dataset.date}`;
+  fragment.appendChild(date);
+  const event = document.createElement('p');
+  event.textContent = `Event: ${element.dataset.event}`;
+  fragment.appendChild(event);
+
+  fragment.append(fragment);
+  return fragment;
+}
+
 export default async function decorate(block) {
   const config = readBlockConfig(block);
   const index = config?.index?.trim();
@@ -12,7 +36,22 @@ export default async function decorate(block) {
   photos.forEach((photo) => {
     const div = document.createElement('div');
     div.classList.add('gallery-photo');
-    div.appendChild(createOptimizedPicture(photo.Image.toLowerCase(), photo.Description, false, [{ width: '350' }]));
+    const button = document.createElement('button');
+    button.classList.add('gallery-button');
+    button.classList.add('js-modal-trigger');
+    button.dataset.description = photo.Description;
+    button.dataset.event = photo.Event;
+    button.dataset.date = photo.Date;
+    button.dataset.image = photo.Image.toLowerCase();
+    button.appendChild(
+      createOptimizedPicture(
+        photo.Image.toLowerCase(),
+        photo.Description,
+        false,
+        [{ width: '350' }],
+      ),
+    );
+    div.appendChild(button);
     block.append(div);
   });
 }

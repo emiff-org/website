e036d381b0dea12534a2d18af12c79328cbbdb7c
oliver <gokohl@gmail.com>
2025-07-02 00:18:32 +0200

    feed
    - update from program-feed
    - exclude tools from query-index

--- blocks/feed/feed.js
+++ blocks/feed/feed.js
@@ -1,27 +1,41 @@
 import getIndexPath from '../../scripts/index-utils.js';
 import { readBlockConfig } from '../../scripts/aem.js';
 import ffetch from '../../scripts/ffetch.js';
+import {
+  createCustomSelect,
+  parseDateTime,
+  getIcon,
+  getCSFilterMap,
+  getFiltersKvMap,
+} from '../../scripts/components-utils.js';
 
-async function fetchItems(config) {
-  const index = config?.index?.trim().toUpperCase();
-  const type = config?.type?.trim().toLowerCase();
-  const limit = parseInt(config?.limit ?? '', 10) || undefined;
-
-  const rawItems = await ffetch(getIndexPath(`INDEX_${index}`)).all();
-  return rawItems
-    .filter((item) => item.type.toLowerCase() === type)
+// expects a map with kv pairs as filter
+function filterItems(items, filters) {
+  const filteredItems = (filters.length === 0) ? items : items.filter((item) => filters.entries()
+    .every(([key, value]) => {
+      if (value.length === 0 || value.toLowerCase().startsWith('all ')) return true;
+      const itemValue = item[key.toLowerCase()]?.toString().toLowerCase().trim();
+      const filterValue = value.toString().toLowerCase().trim();
+      return itemValue === filterValue;
+    }));
+  return filteredItems
     .sort((a, b) => {
       const dateA = parseInt(a.publicationDate || '0', 10);
       const dateB = parseInt(b.publicationDate || '0', 10);
-      return dateB - dateA; // descending
-    })
-    .slice(0, Number.isNaN(limit) ? undefined : limit);
+      return dateA - dateB;
+    });
 }
 
-function renderCardItems(itemsToRender, container) {
+async function fetchItems(config) {
+  const index = config?.index?.trim().toUpperCase();
+  const items = await ffetch(getIndexPath(`INDEX_${index}`)).all();
+  return items;
+}
+
+function renderCardItems(itemsToRender, container, limit) {
   const ul = document.createElement('ul');
 
-  itemsToRender.forEach((loc) => {
+  itemsToRender.slice(0, limit).forEach((loc) => {
     const li = document.createElement('li');
 
     // Image section
@@ -84,10 +98,10 @@ function renderCardItems(itemsToRender, container) {
   return container;
 }
 
-function renderListItems(itemsToRender, container) {
+function renderListItems(itemsToRender, container, limit) {
   container.innerHTML = ''; // Clear previous items
 
-  itemsToRender.forEach((item) => {
+  itemsToRender.slice(0, limit).forEach((item) => {
     const div = document.createElement('div');
     div.classList.add('feed-item');
 
@@ -128,94 +142,61 @@ function renderListItems(itemsToRender, container) {
   return container;
 }
 
-function renderItems(items, layout) {
+function renderFeed(items, layout, limit) {
   const container = document.createElement('div');
 
   if (layout === 'cards') {
     container.classList.add('cards', 'block');
-    renderCardItems(items, container);
+    renderCardItems(items, container, limit);
   } else {
     container.classList.add('feed');
-    renderListItems(items, container);
+    renderListItems(items, container, limit);
   }
   return container;
 }
 
-function createCustomSelect(filter, rawItems, items, onSelect) {
-  const uniqueFilterValues = [...new Set(
-    rawItems.map((item) => item[filter.toLowerCase()]).filter(Boolean),
-  )];
+function renderControls(filters, items, layout, hideFilters) {
+  const container = document.createElement('div');
+  container.classList.add('custom-select-flex');
 
-  const selectWrapper = document.createElement('div');
-  selectWrapper.classList.add('custom-select-wrapper');
-  selectWrapper.style.display = 'flex';
-  selectWrapper.style.alignItems = 'center';
-  selectWrapper.style.gap = '10px';
-
-  const label = document.createElement('label');
-  label.textContent = filter;
-  label.classList.add('custom-select-label');
-  selectWrapper.prepend(label);
-
-  const select = document.createElement('div');
-  select.classList.add('custom-select');
-  select.textContent = 'All';
-
-  const optionsWrapper = document.createElement('div');
-  optionsWrapper.classList.add('custom-options');
-  optionsWrapper.style.display = 'none';
-
-  const createOption = (value) => {
-    const option = document.createElement('div');
-    option.classList.add('custom-option');
-    option.textContent = value;
-    option.addEventListener('click', () => {
-      select.textContent = value;
-      optionsWrapper.style.display = 'none';
-      select.classList.remove('open');
-      const filteredItems = value === 'All'
-        ? items
-        : items.filter((item) => item[filter.toLowerCase()] === value);
-      onSelect(filteredItems);
-    });
-    return option;
-  };
-
-  optionsWrapper.append(createOption('All'));
-  uniqueFilterValues.forEach((value) => {
-    optionsWrapper.append(createOption(value));
+  filters.entries().forEach(([key, value]) => {
+    if (hideFilters.includes(key)) return; // skip hidden filters
+    const selectWrapper = createCustomSelect(
+      key,
+      items,
+      (select) => {
+        const parentBlock = select.closest('.feed');
+        const compFilter = getCSFilterMap(parentBlock); // get latest filters from UI components
+        const filteredItems = filterItems(items, compFilter);
+        const feedBlock = renderFeed(filteredItems, layout);
+        const oldFeedBlock = parentBlock.querySelector('div.feed');
+        if (oldFeedBlock) parentBlock.replaceChild(feedBlock, oldFeedBlock);
+        else parentBlock.append(feedBlock);
+      },
+      value,
+    );
+    container.append(selectWrapper);
   });
-
-  select.addEventListener('click', () => {
-    const isOpen = optionsWrapper.style.display === 'none';
-    optionsWrapper.style.display = isOpen ? 'block' : 'none';
-    select.classList.toggle('open', isOpen);
-  });
-
-  selectWrapper.append(select);
-  selectWrapper.append(optionsWrapper);
-  return selectWrapper;
+  return container;
 }
 
 export default async function decorate(block) {
   const config = readBlockConfig(block);
-  const filter = config?.filter?.trim();
-  const layout = config?.layout?.trim().toLowerCase();
+  const filterValue = config?.filter?.trim();
+  const filters = getFiltersKvMap(filterValue);
+  const hideFiltersValue = config?.hidefilters?.trim();
+  const hideFilters = hideFiltersValue ? hideFiltersValue.split(',').map((val) => val.trim()) : [];
+  const limit = config?.limit?.trim().toLowerCase();
 
   const items = await fetchItems(config);
-  const rawItems = await ffetch(getIndexPath(`INDEX_${config?.index?.trim().toUpperCase()}`)).all();
+  const filteredItems = filterItems(items, filters);
 
   block.textContent = '';
 
-  if (filter) {
-    const selectWrapper = createCustomSelect(
-      filter,
-      rawItems,
-      items,
-      (filteredItems) => renderItems(filteredItems, layout),
-    );
-    block.append(selectWrapper);
+  // accepted layouts: feed, cards
+  const layout = config?.layout?.trim().toLowerCase();
+  if (!Array.isArray(filters) || filters.length !== 0) {
+    block.append(renderControls(filters, items, layout, hideFilters));
   }
-
-  block.append(renderItems(items, layout));
+  block.append(renderFeed(filteredItems, layout, limit));
 }

6609e9fe98b62014510196e97f2eea09a1cbd2e8
oliver <gokohl@gmail.com>
2025-07-10 13:30:59 +0200

    Update index-utils.js
    - locations index fix

--- scripts/index-utils.js
+++ scripts/index-utils.js
@@ -4,7 +4,7 @@ const INDEX_PATHS = {
   INDEX_ALL: 'query-index',
   INDEX_NEWS: 'query-index',
   INDEX_MENTIONS: 'news/mentions-index',
-  INDEX_LOCATIONS: 'locations-index',
+  INDEX_LOCATIONS: 'festival/locations-index',
   INDEX_ENTRIES: 'program/entries-index',
   INDEX_PROGRAM: 'program/program-index',
   INDEX_BLOCKS: 'program/blocks-index',
